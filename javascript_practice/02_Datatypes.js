6bef61aa8c556381d9f242e7690c6b145b823ec2
Dhananjay Bhatkar <69847047+DBhatkar009@users.noreply.github.com>
2024-02-05 10:40:13 +0000

    Data Types of javascript

---
+++ javascript_practice/02_Datatypes.js
@@ -0,0 +1,18 @@
+// Data types are 
+
+
+let value1 = "Shekhar";
+let howMany = 12546;
+
+let nameIs;
+
+let value = true;
+
+console.log(typeof value1); // string
+console.log(typeof howMany);  // number
+console.log(typeof value); // undefine
+console.log(typeof nameIs); // boolean
+
+// symbol 
+// bigInt
+// null
