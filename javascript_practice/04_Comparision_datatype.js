d38dd81a00fcb99e4ab206b5770342d085e60e45
Dhananjay Bhatkar <69847047+DBhatkar009@users.noreply.github.com>
2024-02-06 06:57:59 +0000

    comparison datatypes example practice

---
+++ javascript_practice/04_Comparision_datatype.js
@@ -0,0 +1,16 @@
+let num = 55 + 66;
+console.log(num);
+
+const newNumber = "55" + 10;
+
+console.log(newNumber);
+
+let newNumOne = 66 + "77";
+console.log(newNumOne);
+
+let result = newNumOne > newNumber;
+result = newNumOne < newNumber;
+
+console.log(result);
+
+
