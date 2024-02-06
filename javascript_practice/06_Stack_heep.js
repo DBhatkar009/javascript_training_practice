d62d2dd776a342a54fdd8dbfdfe0642a8e866bb7
Dhananjay Bhatkar <69847047+DBhatkar009@users.noreply.github.com>
2024-02-06 07:29:29 +0000

    Heeps & stacks

---
+++ javascript_practice/06_Stack_heep.js
@@ -0,0 +1,22 @@
+
+
+let newUser = "IamNewBlood"
+
+let newName = newUser;
+
+console.log(newName);
+
+newName = "ItsMehear";
+console.log(newName);
+
+
+let userObj = {
+    email: "Dj@google.com",
+    upi: 'dj@upi'
+}
+
+let userObj2 = userObj;
+console.log(userObj2);
+
+userObj2.email = "Dj@Amazon.com";
+console.log(userObj);
