73e7a368ee8eeefff1c4c48f30da9edf90ed8fbe
Dhananjay Bhatkar <69847047+DBhatkar009@users.noreply.github.com>
2024-02-06 06:14:46 +0000

    String conversion number to sring & string to number

---
+++ javascript_practice/03_String_Conversion.js
@@ -0,0 +1,16 @@
+let userName = "dhananjay";
+
+const newUserName = Number(userName);
+
+console.log(typeof newUserName);
+
+console.log(typeof userName);
+
+
+let newNum = 456125;
+
+const numOne = String(newNum);
+
+console.log(typeof newNum);
+
+console.log(typeof numOne);
