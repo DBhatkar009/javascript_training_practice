70312ffb47d083d144e4d7b26735e0fc008351c4
Dhananjay Bhatkar <69847047+DBhatkar009@users.noreply.github.com>
2024-02-06 06:58:21 +0000

    data types summary javascript

---
+++ javascript_practice/05_Datatype_summary.js
@@ -0,0 +1,44 @@
+
+
+
+// premitive data types
+
+let premitive = ['string', 'number', 'boolean', 'null', 'symbol', 'undefined', 'bigint'];
+
+console.log(premitive);
+
+
+// non premitives data types
+
+let nonPremitive = ['function', 'array', 'object'];
+
+console.log(nonPremitive);
+
+
+//bigint example
+const types = 15456456454545644545n;
+
+console.log(typeof types);
+
+
+//symbol example
+let symbol = Symbol('1254');
+console.log(typeof symbol);
+
+// function declaration example
+const value = function(){
+    console.log("hello world");
+}
+value();
+
+
+
+let objectData = {
+    name: 'Dhananjay',
+    fatherName: 'Shekhar',
+    motherName: 'Shrutika'
+}
+console.log(objectData);
+console.log(typeof objectData);
+console.log(typeof value);
+
